b7008c303327df8b1d3c472c45607fff0dbf9fef
Douglas Sellers <douglas.sellers@gmail.com>
2012-12-28 10:14:58 -0800

    Initial add of rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yukiko::Application.config.secret_token = '8ccc8b9f58ab4480e261bee96b76fddf8e57427033b1027cab237d5d3904a62085f3260f1505759c702298ccbdd17e4c1d5c336f12733f74c2516cb39d6a0b29'
