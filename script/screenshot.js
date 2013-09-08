e2679569c24367373739bf38436345694ab2f975
Douglas Sellers <doug@fitorb.com>
2013-09-07 21:39:26 -0700

    Made it so that there is a lot better error handling

--- script/screenshot.js
+++ script/screenshot.js
@@ -12,33 +12,61 @@ if (phantom.args.length != 4)
 
 var fs = require('fs');
 var page = new WebPage();
-page.onConsoleMessage = function(msg) {
-     console.log(msg);
-};
-page.onResourceRequested = function(resource) {
-//    console.log( "Loading: " + resource.url );
+var number_of_resources_requested = 0;
+var number_of_resources_failed = 0;
+
+page.onResourceRequested = function(resource) 
+{
+    number_of_resources_requested += 1;
 };
 
-page.onResourceReceived = function(resource) {
+page.onResourceReceived = function(resource) 
+{
 //    console.log( "Received: " + resource.status );
 };
 
+page.onResourceError = function(resourceError) 
+{
+    number_of_resources_failed += 1;
+}
+
+page.onLoadFinished = function(status) 
+{
+    console.log( "Resource requested count: " + number_of_resources_requested );
+    console.log( "Resource failed count: " + number_of_resources_failed );
+
+    // if more than 25% of the resources failed to load it is probably a local request
+    // so let's not bother generating the screenshot
+    if( number_of_resources_requested > 0 )
+    {
+        if( (number_of_resources_failed / number_of_resources_requested ) > 0.25 )
+        {
+            phantom.exit();
+        }
+    }
+
+    try
+    {
+        page.render( output_file );
+    } catch( err )
+    {
+    }
+
+    phantom.exit();
+}
 
 page.viewportSize = { top: 0, left: 0, width: browser_width, height: browser_height };
 page.loadImages = true;
 page.settings = { localToRemoteUrlAccessEnabled: true, loadImages: true, javascriptEnabled: true };
+
 page.open( url,
            function (status)
            {
-               console.log( status );
-              page.evaluate(function() {
-                  document.body.bgColor = 'white';
-              });
-               window.setTimeout(function ()
-                                 {
-                                     console.log( page );
-                                     page.evaluate( function() { console.log( document.head.innerHTML ); } );
-                                     page.render( output_file );
-                                     phantom.exit();
-                                 }, 5000); 
+               page.evaluate(function() 
+                             {
+                                 document.body.bgColor = 'white';
+                             });
            } );
+
+// if it takes more than 60 seconds then exit
+window.setTimeout( function () { phantom.exit(); }, 60000 );
